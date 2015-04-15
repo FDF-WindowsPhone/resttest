65f62b2f69984cb47605dfd53b87d9adc1ea0b6f
FdF <fdefrancisco@solidgear.es>
2015-04-15 18:32:43 +0200

    Working on first request

---
+++ RESTTest/RESTTest.Shared/UserControls/UCActions.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace RESTTest.UserControls
+{
+    public sealed partial class UCActions : UserControl
+    {
+        public UCActions()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
