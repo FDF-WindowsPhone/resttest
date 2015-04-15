4239f0e471849718059b55b627e6118892b23cb6
FDFDev <fdfdev@outlook.com>
2015-04-16 01:06:26 +0200

    GET Request gives results

--- RESTTest/RESTTest.Shared/ViewModel/MainViewModel.cs
+++ RESTTest/RESTTest.Shared/ViewModel/MainViewModel.cs
@@ -3,6 +3,9 @@ using System.Net;
 using System.Text;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
+using System;
+using System.Net.Http.Headers;
+using System.Net.Http;
 
 namespace RESTTest.ViewModel
 {
@@ -11,6 +14,19 @@ namespace RESTTest.ViewModel
         public string Url { get; set; }
         public string Method { get; set; }
 
+        private string _result;
+        public string Result { 
+            get
+            {
+                return _result;
+            }
+            set
+            {
+                _result = value;
+                RaisePropertyChanged();
+            }
+        }
+
         #region Run Command
         private RelayCommand _runCommand;
         public RelayCommand RunCommand
@@ -18,27 +34,30 @@ namespace RESTTest.ViewModel
             get { return _runCommand ?? (_runCommand = new RelayCommand(Run)); }
         }
 
-        private void Run()
+        private async void Run()
         {
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url);
-            request.Method = Method;
-            request.ContentType = "application/x-www-form-urlencoded";
-            request.Accept = "Accept=text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+            string URI = Url;
+            string Parameters = Uri.EscapeUriString("");
+
+            HttpClient client = new HttpClient();
+
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, URI);
             /*
-            byte[] _byteVersion = Encoding.ASCII.GetBytes(string.Concat("content=", dlc_content));
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("text/plain"));
+            request.Headers.AcceptCharset.Add(new StringWithQualityHeaderValue("utf-8", 0.7));
+            request.Headers.AcceptLanguage.Add(new StringWithQualityHeaderValue("en-us", 0.5));
+            
+            request.Content = new StreamContent(new MemoryStream(System.Text.Encoding.UTF8.GetBytes(Parameters)));
+            request.Content.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
+             
+            request.Headers.Host = "www.indianrail.gov.in";
+            request.Headers.UserAgent.Add(new ProductInfoHeaderValue("Mozilla", "5.0"));
+            request.Headers.Referrer = new Uri("http://www.indianrail.gov.in/pnr_stat.html");
+            */
+            var result = await client.SendAsync(request);
+            var content = await result.Content.ReadAsStringAsync();
 
-            request.ContentLength = _byteVersion.Length
-
-            Stream stream = request.GetRequestStream();
-            stream.Write(_byteVersion, 0, _byteVersion.Length);
-            stream.Close();
-
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-
-            using (StreamReader reader = new StreamReader(response.GetResponseStream()))
-            {
-                Console.WriteLine(reader.ReadToEnd());
-            }*/
+            Result = content;
         }
 
         #endregion Run Command
