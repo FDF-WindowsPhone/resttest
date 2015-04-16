00c15ad71052aa0d93a0e111c0dc51f8984eae20
FdF <fdefrancisco@solidgear.es>
2015-04-16 17:01:00 +0200

    Completed saving and restoring during a session. No persistance

---
+++ RESTTest/RESTTest.Shared/Models/RTRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RESTTest.Models
+{
+    public enum PROTOCOLS
+    {
+        HTTP,
+        HTTPS
+    }
+
+    public class RTRequest
+    {
+        public string Name { get; set; }
+        public PROTOCOLS Protocol { get; set; }
+        public string Url { get; set; }
+        public int Method { get; set; }
+
+    }
+}
--- RESTTest/RESTTest.Shared/ViewModel/MainViewModel.cs
+++ RESTTest/RESTTest.Shared/ViewModel/MainViewModel.cs
@@ -4,15 +4,61 @@ using System.Text;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Net.Http.Headers;
 using System.Net.Http;
+using Windows.UI.Popups;
+using Windows.UI.Xaml.Controls;
+using RESTTest.Models;
+using WinRTXamlToolkit.Controls;
+using WinRTXamlToolkit.Controls.Extensions;
 
 namespace RESTTest.ViewModel
 {
     public class MainViewModel : ViewModelBase
     {
-        public string Url { get; set; }
-        public string Method { get; set; }
+
+        public MainViewModel()
+        {
+            Protocol = PROTOCOLS.HTTP;
+            Method = 0;
+        }
+
+        private PROTOCOLS _protocol;
+
+        public PROTOCOLS Protocol
+        {
+            get { return _protocol; }
+            set
+            {
+                _protocol = value;
+                RaisePropertyChanged();
+            }
+        }
+        
+        private string _url;
+        public string Url
+        {
+            get { return _url; }
+            set
+            {
+                _url = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private int _method;
+
+        public int Method
+        {
+            get { return _method; }
+            set
+            {
+                _method = value;
+                RaisePropertyChanged();
+            }
+        }
 
         private string _result;
         public string Result { 
@@ -27,6 +73,33 @@ namespace RESTTest.ViewModel
             }
         }
 
+        private string _resultCode;
+        public string ResultCode
+        {
+            get
+            {
+                return _resultCode;
+            }
+            set
+            {
+                _resultCode = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private ObservableCollection<RTRequest> _requestsList = new ObservableCollection<RTRequest>();
+        public ObservableCollection<RTRequest> RequestsList
+        {
+            get { return _requestsList; }
+            set
+            {
+                _requestsList = value;
+                RaisePropertyChanged();
+            }
+        } 
+
+        #region Commands
+
         #region Run Command
         private RelayCommand _runCommand;
         public RelayCommand RunCommand
@@ -36,8 +109,23 @@ namespace RESTTest.ViewModel
 
         private async void Run()
         {
-            string URI = Url;
-            string Parameters = Uri.EscapeUriString("");
+            Result = "";
+            string protocol = "";
+            switch (Protocol)
+            {
+                case PROTOCOLS.HTTPS:
+                    protocol = "https://";
+                    break;
+                case PROTOCOLS.HTTP:
+                    protocol = "http://";
+                    break;
+                default:
+                    protocol = "http://";
+                    break;
+            }
+
+            string URI = string.Format("{0}{1}", protocol, Url);
+            //string Parameters = Uri.EscapeUriString("");
 
             HttpClient client = new HttpClient();
 
@@ -58,13 +146,95 @@ namespace RESTTest.ViewModel
             var content = await result.Content.ReadAsStringAsync();
 
             Result = content;
+            ResultCode = string.Format("STATUS: {0}", result.StatusCode);
         }
 
         #endregion Run Command
 
-        public MainViewModel()
+        #region Clean Command
+        private RelayCommand _cleanCommand;
+        public RelayCommand CleanCommand
         {
+            get { return _cleanCommand ?? (_cleanCommand = new RelayCommand(Clean)); }
+        }
+
+        private async void Clean()
+        {
+            Result = "";
+            ResultCode = "";
+            Url = "";
+            Protocol = PROTOCOLS.HTTP;
+            Method = 0;
+        }
+
+        #endregion Clean Command
+
+        #region Save Command
+        private RelayCommand _saveCommand;
+        public RelayCommand SaveCommand
+        {
+            get { return _saveCommand ?? (_saveCommand = new RelayCommand(Save)); }
+        }
+
+        private async void Save()
+        {
+            InputDialog dialog = new InputDialog();
+            
+            var result = await dialog.ShowAsync(
+                "Attention",
+                "Enter an Identifier for the Connection",
+                "Save",
+                "Cancel");
+
+            switch (result)
+            {
+                case "Save":
+                    string name = dialog.InputText;
+                    RTRequest request = new RTRequest() {Method = Method, Name = name, Protocol = Protocol, Url = Url};
+                    RequestsList.Add(request);
+                    break;
+                case "Cancel":
+                    break;
+            }            
             
         }
+
+        #endregion Save Command
+
+        #region Select Request Command
+        private RelayCommand<ItemClickEventArgs>  _selectRequestCommand;
+        public RelayCommand<ItemClickEventArgs> SelectRequestCommand
+        {
+            get { return _selectRequestCommand ?? (_selectRequestCommand = new RelayCommand<ItemClickEventArgs>(SelectRequest)); }
+        }
+
+        private async void SelectRequest(ItemClickEventArgs args)
+        {
+            RTRequest request = args.ClickedItem as RTRequest;
+            string message = string.Format("Do you want to load {0}", request.Name);
+            MessageDialog dialog = new MessageDialog(message, "Attention!!");
+            dialog.Commands.Add(new UICommand("Yes", new UICommandInvokedHandler(YesCommandHandler), request));
+            dialog.Commands.Add(new UICommand("No", new UICommandInvokedHandler(NoCommandHandler)));
+            dialog.DefaultCommandIndex = 0;
+            dialog.CancelCommandIndex = 1;
+            await dialog.ShowAsync();
+        }
+
+        private void NoCommandHandler(IUICommand command)
+        {
+            // do Nothing
+        }
+
+        private void YesCommandHandler(IUICommand command)
+        {
+            RTRequest request = command.Id as RTRequest;
+            Url = request.Url;
+            Method = request.Method;
+            Protocol = request.Protocol;
+        }
+
+        #endregion Select Request Command
+
+        #endregion Commands
     }
 }
